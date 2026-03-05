2c82820170dd0c53740c66fa8851ed375a0f5d8f
Philippe6ak <philippe.siaka@gmail.com>
2026-03-05 10:03:31 +0000

    added 3d model using three js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import ProductViewer from "./components/ProductViewer";
 
 function App() {
   return (
     <main className="">
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 }
